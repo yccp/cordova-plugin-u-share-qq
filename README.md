d6ccc7a17617c5318465c22f9ce7d86969dabbc9
Yu Chen <yu.chen@live.ie>
2017-09-28 14:23:05 +0800

    first version

---
+++ README.md
@@ -0,0 +1,14 @@
+# 友盟社会化分享 cordova 插件 微信支持
+
+> 请确保已安装 [友盟社会化分享 cordova 插件](https://github.com/yccp/cordova-plugin-u-share.git)
+申请腾讯开放平台账号 [http://open.qq.com](http://open.qq.com)
+
+## 安装
+
+```
+cordova plugin add cordova-plugin-u-share-qq --variable USHARE_QQ_IOS_APP_ID=你的IOS_APP_ID --variable USHARE_QQ_ANDROID_APP_ID=你的ANDROID_APP_ID --variable USHARE_QQ_ANDROID_APP_KEY=你的IOS_APP_KEY --save
+```
+或
+```
+ionic cordova plugin add cordova-plugin-u-share-qq --variable USHARE_QQ_IOS_APP_ID=你的IOS_APP_ID --variable USHARE_QQ_ANDROID_APP_ID=你的ANDROID_APP_ID --variable USHARE_QQ_ANDROID_APP_KEY=你的IOS_APP_KEY
+```
---
+++ src/android/UShareQQ.java
@@ -0,0 +1,30 @@
+package news.chen.yu.ionic;
+
+import org.apache.cordova.*;
+
+import com.umeng.socialize.PlatformConfig;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+
+public class UShareQQ extends CordovaPlugin {
+
+    private Boolean __init = false;
+
+    private void init() {
+        if(!this.__init) {
+            this.__init = true;
+            UShare.mediaList.add(SHARE_MEDIA.WEIXIN);
+            UShare.mediaList.add(SHARE_MEDIA.WEIXIN_CIRCLE);
+            String appKey = preferences.getString("ushare_qq_android_app_id", "");
+            String appSecret = preferences.getString("ushare_qq_android_app_key", "");
+            LOG.d("u-share", "QQ key: " + appKey);
+            LOG.d("u-share", "QQ secret: " + appSecret);
+            PlatformConfig.setQQZone(appKey, appSecret);
+        }
+    }
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        this.init();
+    }
+}
